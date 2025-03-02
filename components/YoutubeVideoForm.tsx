4da003d4720a3dfee27f9f16139fbc852e8c612a
umar-farooq-shaik <umarfarooqshaik665@gmail.com>
2025-03-02 12:31:58 +0530

    length

--- components/YoutubeVideoForm.tsx
+++ components/YoutubeVideoForm.tsx
@@ -5,7 +5,7 @@ import { analyseYoutubeVideo } from "@/actions/analyseYoutubeVideo";
 function YoutubeVideoForm() {
   return (
     
-    <div className="w-full max-w-2x1 mx-auto"> 
+    <div className="w-full max-w-2xl mx-auto"> 
     <Form 
     action={analyseYoutubeVideo} 
     className="flex flex-col sm:flex-row gap-2 items-center" 
@@ -14,7 +14,7 @@ function YoutubeVideoForm() {
     name="url" 
     type="text" 
     placeholder="Enter YouTube URL" 
-    className="flex-3 w-full px-4 py-2 
+    className="flex-1 w-full px-4 py-2 
     text-gray-700 
     bg-white border 
     border-gray-300 rounded-lg focus:outline-none focus:ring-2 
